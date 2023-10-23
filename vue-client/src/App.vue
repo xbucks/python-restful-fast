3f567fb4350c5b1aff1e701495cfb0368bcdd178
Lucky Star <jmy.kot@gmail.com>
2023-10-22 21:26:57 -0400

    update: clean up code base

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,56 +1,50 @@
 <script setup>
-
-
 </script>
-
 
 // Path: vue-client/src/App.vue
 <template>
   <div id="app">
-      <h1 class="main">Notes</h1>
-      <table class="table" border="black">
-        <thead>
-          <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Completed</th>
-              <th>Created Date</th>
-          </tr>
-        </thead>
-          <tbody> 
-              <tr v-for="note in notes" :key="note.id">
-              <td>{{ note.id }}</td>
-                  <td>{{ note.title }}</td>
-                  <td>{{ note.description }}</td>
-                  <td>{{ note.completed }}</td>
-                  <td> {{ note.created_date }}</td>
-              </tr>
-          </tbody>
-      </table>
+    <h1 class="main">Notes</h1>
+    <table class="table" border="black">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Completed</th>
+          <th>Created Date</th>
+        </tr>
+      </thead>
+      <tbody> 
+        <tr v-for="note in notes" :key="note.id">
+        <td>{{ note.id }}</td>
+          <td>{{ note.title }}</td>
+          <td>{{ note.description }}</td>
+          <td>{{ note.completed }}</td>
+          <td> {{ note.created_date }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-
 </template>
-
 
 
 <script>
 import Api from './Api'
 
 export default {
-    name: 'app',
-    data() {
-        return {
-            notes: []
-        }
-    },
-    created() {
-        Api().then(response => {
-            this.notes = response.data
-        })
+  name: 'app',
+  data() {
+    return {
+      notes: []
     }
+  },
+  created() {
+    Api().then(response => {
+      this.notes = response.data
+    })
+  }
 }
-
 
 </script>
 <style scoped>
@@ -83,6 +77,5 @@ export default {
   animation-play-state: running;
   animation: slide-in 1s ease-out 0.5s 1 normal forwards;
   animation-fill-mode: backwards;
-
 }
 </style>
