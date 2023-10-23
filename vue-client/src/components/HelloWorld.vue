92e8198c997837a4c7bf4209383cbbe43c7cf436
Lucky Star <jmy.kot@gmail.com>
2023-10-22 20:26:38 -0400

    fix: clean up code base

--- vue-client/src/components/HelloWorld.vue
+++ vue-client/src/components/HelloWorld.vue
@@ -10,15 +10,10 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
   <!-- <div class="card " >
     <button type="button" @click="count++">count is {{ count }}</button>
   </div> -->
-
-
-
 </template>
-
 
 <style scoped>
 .read-the-docs {
